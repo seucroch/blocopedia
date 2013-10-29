ae59efd7c3e3146f7dbaff023a07e9009359fce9
seucroch <pablo.seuc.rocher@gmail.com>
2013-10-29 10:29:33 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocopedia::Application.config.secret_token = '9cc2531c6feaf72f3d2366e7d24ca5fc7df0c3003dfb2bcfded0a80badf2d34e882eea28266af2d8bf03cfeaf10213a677b349e20c4ebeba352c2e7ecfaef055'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Blocopedia::Application.config.session_store :cookie_store, key: '_blocopedia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Blocopedia::Application.config.session_store :active_record_store
