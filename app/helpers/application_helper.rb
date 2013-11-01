ae73a23e260c2c465e64521bc45e1e8f8c390d87
seucroch <pablo.seuc.rocher@gmail.com>
2013-10-31 18:46:55 -0700

    added validation w/o user option and created wiki partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
    end
+
+    def control_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'control-group error'
+    else
+      content_tag :div, capture(&block), class: 'control-group'
+    end
+  end
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,10 @@
 class Wiki < ActiveRecord::Base
   attr_accessible :body, :title
   belongs_to :user
+
+  default_scope order('created_at DESC')
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+
 end
