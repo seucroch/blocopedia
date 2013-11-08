4437a17e3ac3af99f0895b46b2a2852b6d202133
Jared Tame <jmtame@gmail.com>
2013-11-07 22:48:08 -0800

    model cleanup

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,12 +1,9 @@
 class Relationship < ActiveRecord::Base
-  attr_accessible :collaborator_id, :editor_id
+	attr_accessible :wiki, :collaborator
 
-  belongs_to :editor, class_name: "User"
+  belongs_to :wiki
   belongs_to :collaborator, class_name: "User"
-  validates :editor_id, presence: true
-  validates :collaborator_id, presence: true
 
-  def self.collaborate!(owner, collaborator)
-    Relationship.create(editor_id: owner.id, collaborator_id: collaborator.id)
-  end
+  validates :wiki_id, presence: true
+  validates :collaborator_id, presence: true
 end
